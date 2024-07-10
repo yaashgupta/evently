45d9bc60bf40c1406a620dbcdbff0da5369a9f97
Yash Gupta <gyash0440@gmail.com>
2024-07-11 02:28:56 +0530

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bordet-t'>
+        <div className='flex-center wrapper flex-between flex flex-col gap-4 text-center sm:flex-row '>
+            <Link href="/">
+            <Image
+            src="/assets/images/logo.svg"
+            alt='logo'
+            width={128}
+            height={38}
+            />
+            </Link>
+            <p>2024 Evently. All Rights reserved </p>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
