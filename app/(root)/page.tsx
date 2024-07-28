5ce1e250a57deff42a2747f078532c0f4b357eb2
Yash Gupta <gyash0440@gmail.com>
2024-07-29 03:17:20 +0530

    finish the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,24 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
+
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.action";
+import { SearchParamProps } from "@/types";
 
 import Image from "next/image";
 import Link from "next/link";
 
-
-export default async function Home() {
-const events = await getAllEvents({
-  query :'',
-  category:'',
-  page: 1,
-  limit:6
-})
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
 
   return (
     <>
@@ -42,23 +48,23 @@ const events = await getAllEvents({
       </section>
 
       <section id="events" className="wrapper my-8 flex flex-col md:gap-12">
-        <h2 className="h2-bold"> Trusted by <br /> Thousands of Events</h2>
-        <div className="flex w-full flex-col  gap-5 md:flex-row"> 
-          Search
-          CategoryFilter
-
+        <h2 className="h2-bold">
+          {" "}
+          Trusted by <br /> Thousands of Events
+        </h2>
+        <div className="flex w-full flex-col  gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
         </div>
         <Collection
-        data={events?.data}
-        emptyTitle= "No Events Found"
-        emptyStateSubtext = "Come back later"
-        collectionType="All_Events"
-        limit={6}
-        page={1}
-        totalPages={2}
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={3}
+          page={page}
+          totalPages={events?.totalPages}
         />
-       
-
       </section>
     </>
   );
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,14 +1,23 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.action";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+
+    const orders = await getOrdersByUser({userId , page:ordersPage})
+    const orderedEvents = orders?.data.map((order:IOrder )=> order.event || [])
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
   return (
     <>
       {/* My tickets */}
@@ -20,18 +29,18 @@ const ProfilePage = async () => {
           </Button>
         </div>
       </section>
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
    <Collection
-        data={events?.data}
+        data={orderedEvents}
         emptyTitle= "No event tickets purchased yet"
         emptyStateSubtext = "No worries - plenty of exciting events to explore!"
         collectionType="My_Tickets"
         limit={3}
-        page={1}
+        page={ordersPage}
         urlParamName="ordersPage"
-        totalPages={2}
+        totalPages={orders?.totalPages}
         />
-   </section> */}
+   </section>
 
       {/* Events organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -48,10 +57,10 @@ const ProfilePage = async () => {
           emptyTitle="No event have been created yet"
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
-          limit={6}
-          page={1}
+          limit={3}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
